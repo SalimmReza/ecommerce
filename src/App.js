8b9a44c17f6f893caac1042c334d0e00dbe95da4
SalimmReza <salimnuaa@gmail.com>
2022-10-09 20:49:35 +0600

    nav components set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Main';
+import Order from './components/Order';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
 
 
   const router = createBrowserRouter([
     {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
 
+        {
+          path: 'home',
+          element: <Home></Home>
+        },
+
+
+        {
+          path: 'order',
+          element: <Order></Order>
+        },
+
+        {
+          path: 'about',
+          element: <About></About>
+        }
+
+      ]
     }
   ])
   return (
